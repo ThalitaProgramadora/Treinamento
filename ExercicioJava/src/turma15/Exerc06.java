a894c9f1e8d946216acb6dc9e10063b41da988ba
Thalita Oliveira Silva <thalita.programadora@gmail.com>
2021-01-26 11:16:45 -0300

    exercicio 6 adicionado

--- ExercicioJava/src/turma15/Exerc06.java
+++ ExercicioJava/src/turma15/Exerc06.java
@@ -1,10 +1,27 @@
 package turma15;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Exerc06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x1,x2,y1,y2;
+        double d;
+          Scanner pontos = new Scanner(System.in);
+          
+          System.out.println("Digite o valor de X1: ");
+          x1 = pontos.nextInt();
+          System.out.println("Digite o valor de X2: ");
+          x2 = pontos.nextInt();
+          System.out.println("Digite o valor de Y1: ");
+          y1 = pontos.nextInt();
+          System.out.println("Digite o valor de Y2: ");
+          y2 = pontos.nextInt();
+          
+         d =Math.sqrt((Math.pow((x2-x1), 2)+Math.pow((y2-y1),2)));
+         JOptionPane.showMessageDialog(null,"O resultado da equação é: "+d);
 	}
 
 }
